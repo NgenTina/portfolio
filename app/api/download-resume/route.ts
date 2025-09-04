32a5984dfe4bb37fca0290adc7ad5233876d663b
StrikerEurika <ngentina69@gmail.com>
2025-09-04 23:19:07 +0700

    add new cv - ngentina_cv_data_science_general.pdf

--- app/api/download-resume/route.ts
+++ app/api/download-resume/route.ts
@@ -3,7 +3,7 @@ import fs from "fs";
 import path from "path";
 
 export async function GET(req: NextRequest) {
-  const filePath = path.join(process.cwd(), "public/docs/NgenTina_CV_V2_General.pdf");
+  const filePath = path.join(process.cwd(), "public/docs/ngentina_cv_data_science_general.pdf");
 
   if (!fs.existsSync(filePath)) {
     return new NextResponse("File not found", { status: 404 });
